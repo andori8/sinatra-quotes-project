516155ab0b7f8f0bbebf1ae3d77d6af33ed76bbe
Ryan Do <falcsmike7@gmail.com>
2017-08-17 02:10:35 +0000

    Creating Quotes form and posting routes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,21 @@ class QuotesController < ApplicationController
       redirect "/login"
     end
   end
+
+  get '/quotes/new' do
+    if logged_in?
+      erb :"/quotes/new"
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/quotes' do
+    if params[:content].empty? || params[:author].empty?
+      redirect "/quotes/new"
+    else
+      quote = Quote.create(content: params[:content], author: params[:author], user_id: session[:user_id])
+      redirect "/quotes"
+    end
+  end
 end
