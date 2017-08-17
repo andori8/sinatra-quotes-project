25596cc895a8b42e01c51378605b56c1e9c68bec
Ryan Do <falcsmike7@gmail.com>
2017-08-17 01:48:26 +0000

    created signup page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,10 @@
 class QuotesController < ApplicationController
+
+  get '/quotes' do
+    if logged_in?
+      erb :"/quotes/index"
+    else
+      redirect "/login"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/quotes"
+    else
+      erb :"/users/signup"
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect "/signup"
+    else
+      user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = user.id
+      redirect "/quotes"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/login"
+    end
+  end
 end
