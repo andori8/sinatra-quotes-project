040c7a9e5fcd2eca6dfd6537af506a0c8ac715b9
Ryan Do <falcsmike7@gmail.com>
2017-08-17 02:28:02 +0000

    Edit form and patch route for quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,6 +16,31 @@ class QuotesController < ApplicationController
     end
   end
 
+  get '/quotes/:id' do
+    if logged_in?
+      @quote = Quote.find(params[:id])
+      erb :"/quotes/show"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/quotes/:id/edit' do
+    if logged_in?
+      @quote = Quote.find(params[:id])
+      erb :"/quotes/edit"
+    else
+      redirect "/login"
+    end
+  end
+
+  patch '/quotes/:id' do
+    @quote = Quote.find(params[:id])
+    @quote.update(content: params[:content], author: params[:author])
+    @quote.save
+    redirect "/quotes/#{@quote.id}"
+  end
+
   post '/quotes' do
     if params[:content].empty? || params[:author].empty?
       redirect "/quotes/new"
